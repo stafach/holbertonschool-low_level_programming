461530e1ff5eb6fceaf96662425e0b1de22e622f
stafach <mchermat@outlook.fr>
2025-06-17 07:56:33 +0000

    write a different text if n >,=,< 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,19 +14,19 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive\n")
+		printf("is positive\n");
 	}
 	else
 	{
 		if (n == 0)
 		{
-			printf("is zero\n")
+			printf("is zero\n");
 		}
 		else
 		{
 			if (n < 0)
 			{
-				printf("is negative\n)
+				printf("is negative\n);
 			}
 		}
 	}
