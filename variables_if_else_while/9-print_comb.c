5da681fa574f52dc8fbfb18c6009de8b0c6f31ae
stafach <mchermat@outlook.fr>
2025-06-17 13:59:27 +0000

    Write a program that prints all possible combinations of single-digit numbers, separete with , and space utilising putchar

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,13 +8,20 @@
 int main(void)
 
 {
-	char  n;
+	int  n;
+	{
 
 	for (n = 48; n <= 57; n++)
 	{
 		putchar (n);
+		if (n < 57)
+		{
 		putchar (44);
 		putchar (32);
+	}
+		
+		
+	}
 	}
 	putchar ('\n');
 	return (0);
