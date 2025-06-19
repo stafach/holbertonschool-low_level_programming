1e9c1f949a570ea49dcc7344efbd15991ea5d5f2
chermat mustapha <mchermat@outlook.fr>
2025-06-19 18:08:44 +0200

    table of 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,31 +7,29 @@
 
 void times_table(void)
 {
-int i, j, mul, num;
+int i, j, mul;
 
-num = 9;
-for (i = 0; i < num + 1; i++)
+for (i = 0; i < 10; i++)
 {
-for (j = 0; j < num + 1; j++)
+for (j = 0; j < 10; j++)
 {
 mul = i * j;
 if (mul == 0)
 {
-_putchar(mul + '0');
-_putchar(',');
-_putchar(' ');
+_putchar('0');
 }
 else if (mul < 10)
 {
 _putchar(' ');
-_putchar(mul + '0');
-_putchar(',');
-_putchar(' ');
+_putchar('0');
 }
 else
 {
 _putchar((mul / 10) + '0');
 _putchar((mul % 10) + '0');
+}
+if (j < 9)
+{
 _putchar(',');
 _putchar(' ');
 }
