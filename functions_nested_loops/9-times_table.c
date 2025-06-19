7e866b4c82f500d2a94936cc33afe72be74fc64b
chermat mustapha <mchermat@outlook.fr>
2025-06-19 18:42:55 +0200

    table of 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,11 +14,17 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 mul = i * j;
-if (mul == 0)
+
+if (j == 0)
 {
 _putchar(mul + '0');
 }
-else if (mul < 10)
+else
+{
+_putchar(',');
+_putchar(' ');
+
+if (mul < 10)
 {
 _putchar(' ');
 _putchar(mul + '0');
@@ -28,10 +34,6 @@ else
 _putchar((mul / 10) + '0');
 _putchar((mul % 10) + '0');
 }
-if (j < 9)
-{
-_putchar(',');
-_putchar(' ');
 }
 }
 _putchar('\n');
