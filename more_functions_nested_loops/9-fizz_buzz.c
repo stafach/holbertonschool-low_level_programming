c45a715be9577d4ccb076c47d76d3d288978ba7f
chermat mustapha <mchermat@outlook.fr>
2025-06-23 14:39:24 +0200

    The “Fizz-Buzz test”

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0
+*/
+
+int main(void)
+{
+int i;
+
+for (i =1; i <= 100; i++)
+{
+if (((i % 3) == 0) && ((i % 5) == 0))
+{
+printf("FizzBuzz");
+}
+else if (((i % 3) == 0) && ((i % 5) != 0))
+{
+printf("Fizz");
+}
+else if (((i % 5) == 0) && ((i % 3) != 0))
+{
+printf("Buzz");
+}
+else if (i < 10)
+{
+printf("%d", i);
+}
+else
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+}
+putchar(' ');
+}
+}
