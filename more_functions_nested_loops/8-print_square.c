63efdf867d06eb24bfb671fe673dacad6e971b56
chermat mustapha <mchermat@outlook.fr>
2025-06-23 13:51:53 +0200

    print a square

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,9 @@ if (size <= 0)
 {
 _putchar('\n');
 }
-for (i = 0; i < size; i++) // ligne par ligne
+for (i = 0; i < size; i++)
 {
-for (j = 0; j < size; j++) // caractère par caractère
+for (j = 0; j < size; j++)
 {
 _putchar('#');
 }
