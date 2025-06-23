1bb376c597a07aec1c8296e414fffe264819e45f
chermat mustapha <mchermat@outlook.fr>
2025-06-23 09:46:41 +0200

    check uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_isupper - check if its upper letter
+*@c: car
+*Return: 1 if its upper, 0 otherwise
+*/
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
