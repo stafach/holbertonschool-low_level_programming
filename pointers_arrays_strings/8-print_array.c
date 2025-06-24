2011868118813ea51dbeda0b9c500404d58d398c
chermat mustapha <mchermat@outlook.fr>
2025-06-24 23:05:51 +0200

    Write a function that prints n elements of an array of integers, followed by a new line.

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,14 @@ void print_array(int *a, int n)
 int i;
 for (i = 0; i < n; i++)
 {
-printf("%i,  ", a[i]);
+if (i < n)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
+}
 }
 printf("\n");
 }
