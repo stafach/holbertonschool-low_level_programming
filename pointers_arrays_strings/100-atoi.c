37ef514403336352fab32093fa532a0a2611884f
chermat mustapha <mchermat@outlook.fr>
2025-06-25 13:50:44 +0200

    betty style

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,25 +7,25 @@
 
 int _atoi(char *s)
 {
-int i=0,sign=-1,result=0,found_digit=0;
-while(s[i] != '\0')
+int i = 0, sign = -1, result = 0, found_digit = 0;
+while (s[i] != '\0')
 {
 
-if(s[i]=='-')
+if (s[i] == '-')
 sign *= -1;
 
-if(s[i] >= '0' && s[i] <= '9')
+if (s[i] >= '0' && s[i] <= '9')
 {
 
-found_digit=1;
+found_digit = 1;
 result *= 10;
-result -= (s[i]-'0');
+result -= (s[i] - '0');
 
 }
-else if(found_digit == 1)
+else if (found_digit == 1)
 break;
 i++;
 }
-return sign*result;
+return (sign *result);
 }
 
