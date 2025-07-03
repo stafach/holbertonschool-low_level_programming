00266580904e57c7d38db548f94672461b866201
chermat mustapha <mchermat@outlook.fr>
2025-07-03 17:16:48 +0200

    change if condition

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,22 +11,20 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i;
 	int j;
-	int found = 0;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
 			{
-				if (haystack[j] == needle[i])
-				{	
-					return (&haystack[j]);
-					found = 1;
-					break;
-				}
-
-			}
-			if (!found)
 				break;
+			}
+		}
+
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+
 	}
 	return (0);
 }
