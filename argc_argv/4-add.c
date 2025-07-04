6010f2b412ff6cb358445c0a5b959c0e2d72edec
chermat mustapha <mchermat@outlook.fr>
2025-07-04 09:28:40 +0200

    Write a program that adds positive numbers. If no number is passed to the program, print 0, followed by a new line. If one of the number contains symbols that are not digits, print Error, followed by a new line, and return 1.

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - Entry point
+*@argc: the nomber of arguments
+*@argv: the array of the arguments
+*Return: 1 if Error or 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0)
+			printf("0\n");
+
+		else if (atoi(argv[i]) < 0)
+		{
+			printf("Error\n");
+			return (EXIT_FAILURE);
+		}
+		else
+		{
+			sum += atoi(argv[i]);
+		}
+	}
+	 printf("%d\n", sum);
+	return (0);
+}
