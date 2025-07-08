b6eb9ca5dc9428be24bfdb75dc53ae75f0b0ef7c
chermat mustapha <mchermat@outlook.fr>
2025-07-09 00:23:52 +0200

    change int by char for ptr

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int totalsize = nmemb * size;
-	unsigned int *ptr = malloc(sizeof(int) * totalsize);
+	char *ptr = malloc(totalsize);
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
