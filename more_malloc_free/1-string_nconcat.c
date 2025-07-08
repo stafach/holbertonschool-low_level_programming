0090715cfb4ce2eca174e47ef53771639d6637ab
chermat mustapha <mchermat@outlook.fr>
2025-07-08 21:10:38 +0200

    write a function that concatenates two strings if the second is less than or equal to n

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+**string_nconcat - concatenates two strings if < n
+*@s1: 1st string
+*@s2: 2nd string
+*@n: bytes of s2
+*Return: NULL or a pointer with the string
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+	int j;
+	int len = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len] != '\0')
+		len++;
+
+	str = malloc(sizeof(char) * (len + 1 + n));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (j = 0; j < len; j++)
+		str[j] = s1[j];
+
+	for (i = 0; i <= n; i++)
+	{
+		str[j] = s2[i];
+		j++;
+	}
+
+	str[j] = '\0';
+	return (str);
+}
