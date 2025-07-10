65039c1d59362e8bf03c7514b0684c207e25ac27
chermat mustapha <mchermat@outlook.fr>
2025-07-10 15:21:26 +0200

    add condition if name, owner or age are NULL

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,10 +13,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(struct dog));
 	
-	if (name == NULL || age == 0
-		|| owner == NULL)
-		return (NULL);
-
 	if (new_dog == NULL)
 		return (NULL);
 
