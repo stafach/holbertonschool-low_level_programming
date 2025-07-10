23d342376f3ea72adaea9a44d0fdcba486bebfe7
chermat mustapha <mchermat@outlook.fr>
2025-07-10 16:40:02 +0200

    add a malloc for save name and owner

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,6 +12,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(struct dog));
+	dog_t *newname;
+	dog_t *newowner;
 
 	if (new_dog == NULL)
 		return (NULL);
@@ -19,6 +21,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
+	
+	newname = malloc(sizeof(owner));
+	newowner = malloc(sizeof(name));
+	
+	if (newname == NULL)
+		return (NULL);
+	
+	if (newowner == NULL)
+		return (NULL);
 
 	return (new_dog);
 }
