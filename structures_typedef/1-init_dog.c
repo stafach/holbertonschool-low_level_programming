cd67fa0494255c2e36789170dd6d6aec403d2ad1
chermat mustapha <mchermat@outlook.fr>
2025-07-10 12:17:48 +0200

    add condition if d is NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include "dog.h"
-
+#include <stdlib.h>
 /**
 *init_dog - define the dog's informations
 *@d: the structure
@@ -12,7 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
