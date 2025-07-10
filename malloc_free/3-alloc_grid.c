f5c4664b5be41f0aa0ab4ecdcae772f4e6b2cd9f
chermat mustapha <mchermat@outlook.fr>
2025-07-10 18:35:42 +0200

    betty style 3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	mem = malloc(sizeof(int*) * height);
+	mem = malloc(sizeof(int *) * height);
 	if (mem == NULL)
 		return (NULL);
 
