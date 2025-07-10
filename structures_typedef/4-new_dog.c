262de8a87422a2d0b85864b9b671726616620d6a
chermat mustapha <mchermat@outlook.fr>
2025-07-10 17:31:20 +0200

    add copy off name and owner

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,13 +12,42 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(struct dog));
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int i, j;
 
 	if (new_dog == NULL)
 		return (NULL);
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	while (name[len1] != '\0')
+		len1++;
 
+	new_dog->name = malloc(len1 + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < len1; i++)
+			new_dog->name[i] = name[i];
+	}
+	while (owner[len2] != '\0')
+		len2++;
+
+	new_dog->owner = malloc(len2 + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	else
+	{
+		for (j = 0; j < len2; j++)
+			new_dog->owner[j] = owner[j];
+	}
+	new_dog->age = age;
 	return (new_dog);
 }
