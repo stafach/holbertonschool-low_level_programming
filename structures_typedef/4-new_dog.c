518ff913e9023562cebe9a3e4ce544736c879db3
chermat mustapha <mchermat@outlook.fr>
2025-07-10 15:07:11 +0200

    Write a function that creates a new dog.

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+**new_dog - creates a new dog.
+*@name: name of the new dog
+*@age: age of the new dog
+*@owner: owner of the new dog
+*Return: NULL
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(struct dog));
+
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
+}
