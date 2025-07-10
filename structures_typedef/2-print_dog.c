050d02edf52240bc262f1768c573fd4e119263cd
chermat mustapha <mchermat@outlook.fr>
2025-07-10 17:56:38 +0200

    add return if d = NULL and change age condition

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -10,18 +10,24 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		printf("\n");
+		return;
+	}
 
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
+	else
+		printf("Name; %s\n", d->name);
 
-	if (d->age == 0.0)
+	if (d->age < 0.0)
 		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
 }
