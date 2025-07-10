4e37212a7b2c3d4f5ca4e0bc2d1939dfaf2805d0
chermat mustapha <mchermat@outlook.fr>
2025-07-10 18:11:55 +0200

    add total, change order of declaration

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,16 +11,20 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr = malloc(sizeof(size) * nmemb);
+	unsigned int *ptr;
 	unsigned int i;
+	unsigned int total;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	total = size * nmemb;
+	ptr = malloc(total);
+
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < total; i++)
 		ptr[i] = 0;
 
 	return (ptr);
