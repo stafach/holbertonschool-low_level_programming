f2c7054015042749f171a4383761acfa792b6fc8
chermat mustapha <mchermat@outlook.fr>
2025-07-10 15:22:59 +0200

    betty style for task 4 : new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,13 +12,13 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog = malloc(sizeof(struct dog));
-	
+
 	if (new_dog == NULL)
 		return (NULL);
 
 	new_dog->name = name;
 	new_dog->age = age;
 	new_dog->owner = owner;
-	
+
 	return (new_dog);
 }
