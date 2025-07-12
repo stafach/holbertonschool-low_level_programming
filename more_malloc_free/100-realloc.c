3c6e2e931a56905ba6211783c3d557b1ad77be80
chermat mustapha <mchermat@outlook.fr>
2025-07-12 15:53:47 +0200

    write realloc

---
+++ more_malloc_free/100-realloc.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+**_realloc - reallocates a memory block
+*@ptr: the pointer to reallocate
+*@old_size: the old allocate mem
+*@new_size: the new allocate mem
+*Return: NULL or ptr
+*/
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	char *src;
+	char *dest;
+	unsigned int i;
+	unsigned int min;
+
+	if (new_size == 0)
+	{
+		if (ptr != NULL)
+			free(ptr);
+
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (ptr == NULL)
+		return (new_ptr);
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+	if (old_size < new_size)
+		min = old_size;
+
+	else
+		min = new_size;
+
+	src = (char *) ptr;
+	dest = (char *) new_ptr;
+
+	for (i = 0; i < min; i++)
+		dest[i] = src[i];
+
+	free(ptr);
+	return (new_ptr);
+}
