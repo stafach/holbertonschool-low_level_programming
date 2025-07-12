f8d98c4227282773a8befaa886df2d8e5165a471
chermat mustapha <mchermat@outlook.fr>
2025-07-12 16:19:46 +0200

    betty style realloc and change condition if ptr = NULL

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -20,7 +20,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0 && ptr == NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -33,7 +33,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_ptr == NULL)
 	{
-		free (new_ptr);
+		free(new_ptr);
 		return (NULL);
 	}
 
