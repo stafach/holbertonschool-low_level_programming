815f4a5422a692fba59ae79f6f585cd3839933a9
chermat mustapha <mchermat@outlook.fr>
2025-07-14 23:54:05 +0200

    write a function that finds the place of an integer, defined by a function, in an array

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+*int_index - searches for an integer in an array.
+*@array: the array of integer
+*@size: the size of the array
+*@cmp: the function that decide number to search
+*Return: -1 or the place of the number in the array
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 ||array == NULL
+		|| cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
