e1eb5ee70e7ce08446944cc8cfa28ca76b4e05a1
chermat mustapha <mchermat@outlook.fr>
2025-07-16 22:59:35 +0200

    Write a variadic function that print all number she takes in parameters and separate them by a string we give

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+*print_numbers - print all the parameters
+*@separator: the separator between numbers
+*@n: number of parameters
+*Return: Nothing
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list num;
+
+	va_start(num, n);
+
+	if (separator == NULL)
+		return;
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(num, int));
+
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
