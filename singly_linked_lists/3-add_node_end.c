c74ae0abdf87e863f590e8072fcfd2915a9834bf
chermat mustapha <mchermat@outlook.fr>
2025-07-28 16:44:01 +0200

    add the condition if new_node->str is NULL

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -30,16 +30,23 @@ int _strlen(const char *str)
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *tmp;
-
-	new_node = malloc(sizeof(list_t));
+	list_t *tmp;
+	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	new_node->len = _strlen(str);
-	new_node->str = strdup(str);
 	new_node->next = NULL;
 
 	if (*head != NULL)
