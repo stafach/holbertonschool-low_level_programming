1f3634651b0f767a53335e383bf3040d189cbf64
chermat mustapha <mchermat@outlook.fr>
2025-07-28 11:14:45 +0200

    create a function that print all the element of a linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+*print_list - prints all the elements of a linked list
+*@h: the list
+*Return: NULL or the number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	const list_t *node = malloc(sizeof(list_t));
+
+	if (node == NULL)
+		return (0);
+
+	for (node = h; node != NULL; node = node->next)
+	{
+		if (node->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			i++;
+		}
+		else
+		{
+			printf("[%d] %s\n", node->len, node->str);
+			i++;
+		}
+	}
+	return (i);
+}
