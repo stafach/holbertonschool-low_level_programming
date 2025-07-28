1741126fcdff7261fdd14a8083d7738e9e9319bd
chermat mustapha <mchermat@outlook.fr>
2025-07-28 16:39:42 +0200

    change the order of new_node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -37,16 +37,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
 
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-
-	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
+	new_node->str = strdup(str);
 	new_node->next = NULL;
 
 	if (*head != NULL)
