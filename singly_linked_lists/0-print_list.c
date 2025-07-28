849cdd2cb41dfb1ba72ed1cfbd98b42ff703766a
chermat mustapha <mchermat@outlook.fr>
2025-07-28 11:25:48 +0200

    changed the place of i++ to have the number of nodes

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,17 +14,15 @@ size_t print_list(const list_t *h)
 	if (node == NULL)
 		return (0);
 
-	for (node = h; node != NULL; node = node->next)
+	for (node = h; node != NULL; node = node->next, i++)
 	{
 		if (node->str == NULL)
 		{
 			printf("[0] (nil)\n");
-			i++;
 		}
 		else
 		{
 			printf("[%d] %s\n", node->len, node->str);
-			i++;
 		}
 	}
 	return (i);
