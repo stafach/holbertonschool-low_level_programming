1b37a33ef57a8e2ea71023d164184910df24ca59
chermat mustapha <mchermat@outlook.fr>
2025-07-29 23:36:30 +0200

    add condition if idx = 0, add tmp != NULL, add condition if tmp is longer than the list, add new link to tmp->next

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,8 +13,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *tmp;
 	unsigned int j = 0;
 
-	if (h == NULL || *h == NULL)
+	if (h == NULL)
 		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
 
@@ -25,17 +28,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 	new_node->prev = NULL;
 
+	if (*h == NULL)
+
 	tmp = *h;
-	while (j < idx - 1)
+	while (tmp != NULL && j < idx - 1)
 	{
 		tmp = tmp->next;
 		j++;
 	}
-	if (tmp == NULL)
-		return (NULL);
 
+	if (tmp == NULL || tmp->next == NULL)
+	{
+		if (i + 1 == idx)
+			return (add_dnodeint_end(h, n));
+
+		free(new_node);
+		return (NULL);
+	}
 	new_node->next = tmp->next;
 	tmp->next = new_node;
 	new_node->prev = tmp;
+	tmp->next->prev = new_node;
 	return (new_node);
 }
