d42c68add362dd66df08fc66f6e6faa04e061e14
chermat mustapha <mchermat@outlook.fr>
2025-07-29 14:52:00 +0200

    delete the condition if index if NULL

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int n = 0;
 
-	if (head == NULL || index == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	while (n < index)
