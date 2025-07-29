0a6d42024411f9ab5082c108fd631eb8640d29b5
chermat mustapha <mchermat@outlook.fr>
2025-07-29 10:56:09 +0200

    change new_node->prev to NULL

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = new_node;
+	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
 
