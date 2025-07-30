88471a87387a2400994fc59f13acd6a3bdbd345c
chermat mustapha <mchermat@outlook.fr>
2025-07-30 22:43:06 +0200

    add .h to stdlib

--- file_io/main.h
+++ file_io/main.h
@@ -2,8 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
